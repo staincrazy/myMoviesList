00f8581a885a0e848b2a665afb030e007e399663
Reanold Piatrenka <ronny812@gmail.com>
2022-12-06 21:14:08 +0200

    Add Create, Read

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+
+
+def createDatabase():
+    connection = sqlite3.connect("movies.db")
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Movies (Title TEXT, Director TEXT, Score INT)''')
+    connection.commit()
+    connection.close()
+
+
+def connect_to_db():
+    connection = sqlite3.connect("movies.db")
+    connection.row_factory = sqlite3.Row
+    return connection
+
 
 app = Flask(__name__)
 
@@ -10,9 +26,34 @@ def main():
 
 @app.route("/tables.html")
 def tables():
-    return render_template("tables.html")
+    conn = connect_to_db()
+    movies = conn.execute('SELECT * FROM Movies').fetchall()
+    conn.close()
+    return render_template("tables.html", movies=movies)
 
 
-@app.route("/records.html")
+@app.route("/records.html", methods=["GET"])
 def records():
     return render_template("records.html")
+
+
+@app.route("/records.html", methods=["POST"])
+def addRecord():
+    title = request.form["title"]
+    director = request.form["director"]
+    score = request.form["score"]
+
+    conn = connect_to_db()
+    try:
+        cur = conn.cursor()
+        sqlite3_insert_query = """INSERT INTO MOVIES (Title,Director,Score) VALUES (?,?,?)"""
+        data_tuple = (title, director, score)
+        cur.execute(sqlite3_insert_query, data_tuple)
+        conn.commit()
+
+    except:
+        conn.rollback()
+
+    finally:
+        conn.close()
+        return render_template("records.html")
